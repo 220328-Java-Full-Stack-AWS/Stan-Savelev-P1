f4f8af1a5fef4d455518f7f9ac4dd8cbd0253f40
Stan Ronin <sronin@protonmail.com>
2022-04-17 22:01:55 -0400

    Created all necessary tables. Beginning work on DOA's

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -10,29 +10,31 @@ import java.sql.SQLException;
 public class Driver {
 
     public static void main(String[] args) {
-//        AuthService as = new AuthService();
-//        Scanner loginInput = new Scanner(System.in);
-//
-//        System.out.println("Welcome to the Employee Reimbursement System");
-//        System.out.println("To begin, enter your username");
-//        String username = loginInput.next();
-//
-//        System.out.println("Hello, " + username + ".");
-//        System.out.println("Enter your password");
-//        String password = loginInput.next();
-        // Testing scanner
-//        System.out.println("Username: " + username + "\n" + "Password: " +  password);
-        // Beyond this point, the login method found in AuthService will be invoked to check the
-        //database with the provided credentials.
-        //as.login(username, password)
-        //If the user is not in the database, prompt them to either register or validate credentials.
-        //I may include a change password method if time allows.
-//        loginInput.close();
-        //vv Testing connection vv
+        AuthService as = new AuthService();
+        Scanner loginInput = new Scanner(System.in);
+
         try{
             Connection conn = ConnectionManager.getConnection();
         }catch (Exception e){
             e.printStackTrace();
         }
+
+        System.out.println("Welcome to the Employee Reimbursement System");
+        System.out.println("To begin, enter your username");
+        String username = loginInput.next();
+
+        System.out.println("Hello, " + username + ".");
+        System.out.println("Enter your password");
+        String password = loginInput.next();
+        // Testing scanner
+        System.out.println("Username: " + username + "\n" + "Password: " +  password);
+        // Beyond this point, the login method found in AuthService will be invoked to check the
+        //database with the provided credentials.
+        as.login(username, password);
+        //If the user is not in the database, prompt them to either register or validate credentials.
+        //I may include a change password method if time allows.
+        loginInput.close();
+        //vv Testing connection vv
+
     }
 }
