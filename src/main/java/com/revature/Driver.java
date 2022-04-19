bedc9644f8325e82798b53a6db88d759cf2840dc
Stan Ronin <sronin@protonmail.com>
2022-04-19 13:41:20 -0400

    Finishing getByUsername method and removed receipt column in the ers_reimbursment table.

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -2,6 +2,7 @@ package com.revature;
 
 import com.revature.services.AuthService;
 import com.revature.util.ConnectionManager;
+import com.revature.repositories.UserDAO;
 import java.util.Scanner;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -11,6 +12,8 @@ public class Driver {
 
     public static void main(String[] args) {
         AuthService as = new AuthService();
+        UserDAO foundUser = new UserDAO();
+
         Scanner loginInput = new Scanner(System.in);
 
         try{
@@ -22,15 +25,21 @@ public class Driver {
         System.out.println("Welcome to the Employee Reimbursement System");
         System.out.println("To begin, enter your username");
         String username = loginInput.next();
+        //Testing userByUsername method.
+        try{
+            System.out.println(foundUser.getByUsername(username));
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
 
-        System.out.println("Hello, " + username + ".");
-        System.out.println("Enter your password");
-        String password = loginInput.next();
+//        System.out.println("Hello, " + username + ".");
+//        System.out.println("Enter your password");
+//        String password = loginInput.next();
         // Testing scanner
-        System.out.println("Username: " + username + "\n" + "Password: " +  password);
+//        System.out.println("Username: " + username + "\n" + "Password: " +  password);
         // Beyond this point, the login method found in AuthService will be invoked to check the
         //database with the provided credentials.
-        as.login(username, password);
+//        as.login(username, password);
         //If the user is not in the database, prompt them to either register or validate credentials.
         //I may include a change password method if time allows.
         loginInput.close();
--- src/main/java/com/revature/repositories/UserDAO.java
+++ src/main/java/com/revature/repositories/UserDAO.java
@@ -1,7 +1,9 @@
 package com.revature.repositories;
 
 import com.revature.models.User;
-
+import com.revature.models.Role;
+import com.revature.util.ConnectionManager;
+import java.sql.*;
 import java.util.Optional;
 
 public class UserDAO {
@@ -9,9 +11,32 @@ public class UserDAO {
     /**
      * Should retrieve a User from the DB with the corresponding username or an empty optional if there is no match.
      */
-    public Optional<User> getByUsername(String username) {
-        return Optional.empty();
+    public Optional<User> getByUsername(String username) throws SQLException {
+            Connection conn = ConnectionManager.getConnection();
+            String SQL = "SELECT * FROM ers_users WHERE ers_username = ?";
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
+            pstmt.setString(1,username);
+            ResultSet rs = pstmt.executeQuery();
+
+            User tempUser = new User();
+
+            if (rs.next()) {
+                tempUser.setId(rs.getInt("ers_user_id"));
+                tempUser.setUsername(rs.getString("ers_username"));
+                tempUser.setPassword(rs.getString("ers_password"));
+                int roleId = rs.getInt("user_role_id");
+                if (roleId == 1) {
+                    tempUser.setRole(Role.EMPLOYEE);
+                } else {
+                    tempUser.setRole(Role.FINANCE_MANAGER);
+                }
+                return Optional.of(tempUser);
+            }
+            return Optional.empty();
     }
+
+
+
 
     /**
      * <ul>
@@ -21,6 +46,7 @@ public class UserDAO {
      * </ul>
      */
     public User create(User userToBeRegistered) {
+
         return userToBeRegistered;
     }
 }
